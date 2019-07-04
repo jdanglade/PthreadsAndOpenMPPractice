487f0a0a5671159b1425fd1417bb42cbdbc22a8f
Justin Danglade <jdanglade@gmail.com>
2019-07-04 14:56:22 -0400

    Tasks 1.1 and 1.2 complete

--- Makefile
+++ Makefile
@@ -1,18 +1,23 @@
-SRC_1 = Pthread_NoSynchronization.c
+SRC_1 = Pthread_Synchronization.c
 APP_1 = Pthread_NoSync
+APP_2 = Pthread_WithSync
 CC_1 = gcc
 CC_2 = gcc-9
 FLAGS_1 = -o
+FLAGS_2 = -DPTHREAD_SYNC=1
 PTHREAD = -lpthread
 OPENMP = -fopenmp
 
 .PHONY: all clean
 
-all: $(APP_1)
+all: $(APP_1) $(APP_2)
 
 $(APP_1): $(SRC_1)
 	$(CC_1) $(FLAGS_1) $(APP_1) $(SRC_1) $(PTHREAD)
 
+$(APP_2): $(SRC_1)
+	$(CC_1) $(FLAGS_2) $(FLAGS_1) $(APP_2) $(SRC_1) $(PTHREAD)
+
 clean:
 	$(RM) *.o *.out core *~
-	$(RM) $(APP_1)
+	$(RM) $(APP_1) $(APP_2)
---
+++ Pthread_Synchronization.c
@@ -0,0 +1,124 @@
+/*
+COP4610 – Principles of Operating Systems – Summer C 2019
+Please use OpenMP directives and OpenMP synchronization primitives ONLY as
+appropriate.
+
+Prof. Jose F. Osorio
+Student: Justin Danglade – 4552415
+Project: Multithreaded Programming
+Specs:
+Due Date: 07/8/2019 by 11:55pm
+Module Name:
+
+I Certify that this program code has been written by me
+and no part of it has been taken from any sources.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <time.h>
+#include <pthread.h>
+#define NUMINIT 0
+typedef enum {false, true} bool;
+
+bool validateCommandInput(int, const char * [], long int *);
+void simpleThread(long int threadID);
+void * threadFunction(void * threadID);
+int sharedVariable = NUMINIT;
+#ifdef PTHREAD_SYNC
+pthread_mutex_t lock;
+pthread_barrier_t barrier;
+#endif
+
+int main(int argv, const char * argc[])
+{
+    char * message;
+    long int convert = NUMINIT;
+    long int iterator = NUMINIT;
+
+    #ifdef PTHREAD_SYNC
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init has failed\n");
+        return NUMINIT + 1;
+    }
+    #endif
+    if(validateCommandInput(argv, argc, &convert))
+    {
+        srand (time(NULL));
+        #ifdef PTHREAD_SYNC
+        pthread_barrier_init(&barrier, NULL, convert);
+        #endif
+        pthread_t threads[convert];
+        for(iterator = NUMINIT; iterator < convert; iterator++)
+        {
+            pthread_create(&threads[iterator], NULL, threadFunction, (void *)iterator);
+        }
+        for(iterator = NUMINIT; iterator < convert; iterator++)
+        {
+            pthread_join(threads[iterator], NULL);
+        }
+    }
+    else
+    {
+        message = "Invalid input";
+        printf("%s\n", message);
+        return NUMINIT + 1;
+    }
+    #ifdef PTHREAD_SYNC
+    pthread_mutex_destroy(&lock);
+    pthread_barrier_destroy(&barrier);
+    #endif
+    return NUMINIT;
+}
+
+bool validateCommandInput(int inputLength, const char * input[], long int * convert)
+{
+    bool isInt = false;
+    char * endPoint;
+    if(inputLength > 1)
+    {
+        *convert = atol(input[NUMINIT + 1]);
+        if(NUMINIT != *convert)
+        {
+            isInt++;
+        }
+    }
+    return isInt;
+}
+
+void simpleThread(long threadID)
+{
+    int num, val = NUMINIT;
+    for(num = NUMINIT; num < 20; num++)
+    {
+        if (rand() > RAND_MAX / 2)
+        {
+            usleep(10);
+        }
+        #ifdef PTHREAD_SYNC
+        pthread_mutex_lock(&lock);
+        #endif
+        val = sharedVariable;
+        printf("*** thread %ld sees value %d\n", threadID, val);
+        sharedVariable = val + 1;
+        #ifdef PTHREAD_SYNC
+        pthread_mutex_unlock(&lock);
+        #endif
+    }
+    #ifdef PTHREAD_SYNC
+    pthread_barrier_wait(&barrier);
+    #endif
+    val = sharedVariable;
+    printf("Thread %ld sees final value %d\n", threadID, val);
+}
+
+void * threadFunction(void * threadID)
+{
+    long int tID = NUMINIT;
+    tID = (long int) threadID;
+    simpleThread(tID);
+    return threadID;
+}
